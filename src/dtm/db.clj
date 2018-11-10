72126c8fb3dc019cc9e1cff282332543e2f43fa6
Peter J. Makumbi <peter.makumbi@yahoo.com>
2018-11-10 12:50:25 +0100

    Added schema and test data. Implemented find

--- src/dtm/db.clj
+++ src/dtm/db.clj
@@ -2,12 +2,12 @@
   (:require [datomic.api :as d]
             [mount.core :refer [defstate]]))
 
+
 (def hhs-schema (read-string (slurp "src/dtm/hhs-schema.edn")))
+(def ch6-parts  (read-string (slurp "resources/ch6-partitions.edn")))
 (def ch6-schema (read-string (slurp "resources/ch6-schema.edn")))
 (def ch6-data   (read-string (slurp "resources/ch6-data.edn")))
-(def config (read-string (slurp "resources/config.edn") ))
-
-
+(def config     (read-string (slurp "resources/config.edn") ))
 
 ;; DB URL
 (def db-uri "datomic:mem://tutorial")
@@ -30,6 +30,11 @@
 (defstate conn :start (connet-db)
                :stop (disconnect-db conn))
 
+
+;; Oartitions schema
+(defn transact-partitions
+  []
+  (d/transact conn ch6-parts))
 
 ;; Datomic is that everything is considered data,
 ;; including the definition of the schema for your database.
@@ -62,3 +67,41 @@
   "Return firs and last name of all users"
   []
   (d/q all-users (d/db conn)))
+
+;; Query retunrs all users who have a friend
+;; with a given name.
+(def users-friend
+  '[:find ?e ?first-name ?last-name
+    :in $ ?friend-name
+    :where
+    [?e :user/first-name ?first-name]
+    [?e :user/last-name ?last-name]
+    [?e :user/friend ?f]
+    [?f :user/first-name ?friend-name]])
+
+(defn friend-of
+  "Returns all users who are freinds with a 
+  a user with the given first name"
+  [friend-name]
+  (d/q users-friend
+       (d/db conn)
+       friend-name))
+
+
+
+;; Query returns all data of a user
+;; who has the goven name
+(def name-query '[:find ?e ?f ?l ?m
+                  :in $ ?f
+                  :where
+                  [?e :user/first-name ?f]
+                  [?e :user/last-name ?l]
+                  [?e :user/email ?m]])
+
+
+(defn find-by-name
+  "Find a user by the given name. Can be first or last name"
+  [name]
+  (d/q name-query
+       (d/db conn)
+       name))
